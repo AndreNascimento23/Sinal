00071b60ef987566f3ba45bfe18a6614243ec18a
André Nascimento <andre4.20nascimento@outlook.com>
2023-04-01 19:20:09 -0300

    Concluido

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();int numero;
+
+Console.WriteLine("--- Positivo ou Negativo ---\n");
+Console.Write("Digite um numero: ");
+numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero < 0 )
+{
+    Console.WriteLine("\nNegativo");
+}
+
+else if (numero > 0)
+ {
+    Console.WriteLine("\nPositivo");
+ }   
+
+else 
+{
+    Console.WriteLine("Zero");
+}
